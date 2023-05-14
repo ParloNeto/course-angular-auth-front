1d3d9c2a76da6f210288b47753fae721b6951d4c
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-05-14 18:59:55 -0300

    Fazendo a autenticação do token no LocalStorage

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, map, catchError, throwError } from 'rxjs';
 
 @Injectable({
@@ -9,16 +10,25 @@ export class AuthService {
 
   private readonly url: string = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   public sign(payLoad: {email: string, password: string}): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payLoad).pipe(
-      map((res) => console.log(res)),
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payLoad).pipe(
+      map((res) => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', JSON.stringify(res.token));
+        return this.router.navigate(['admin']);
+      }),
       catchError((err) => {
        if (err.error.message) return throwError(() => err.error.message);
        return throwError(() => "No momento não estamos conseguindo validar estes dados, tente novamente mais tarde!")
       } 
      )
     );
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
   }
 }
